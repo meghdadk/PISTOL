dda38b0759e7b0368bcd28ed96782fd2afece5e8
Meghdadk <mk2296@cam.ac.uk>
2024-12-19 11:59:22 +0000

    latest lrs

--- slurm_jobs.sh
+++ slurm_jobs.sh
@@ -2,6 +2,7 @@ conda activate pistol
 
 model_families=("llama2-7b-chat" "gemma-7b-it" "llama3-8b-instruct" "Qwen2-7B-Instruct")
 pretrained_model_paths=("meta-llama/Llama-2-7b-chat-hf" "google/gemma-7b-it" "meta-llama/Meta-Llama-3-8B-Instruct" "Qwen/Qwen2-7B-Instruct")
+
 num_runs=(1 2 3)
 
 dataset_names=("tofu_full" "pistol_sample1")
@@ -9,8 +10,14 @@ data_paths=("data/tofu_full.json" "data/sample_data_1.json")
 forget_types=("author1" "forget_AB")
 forget_edges=("[\"author_1\"]" "[\"A_B\"]")
 
-pistol_lrs=(1.5e-5 1.5e-5 5e-6 1.5e-5)
-tofu_lrs=(2e-5 2e-5 1e-5 1.5e-5)
+pistol_lrs=(1.75e-5 1.5e-5 1.25e-5 2e-5)
+tofu_lrs=(2.5e-5 1.05e-5 1.75e-5 4e-5)
+#llama2_llama3_pistol_lrs=(1.75e-5 1.25e-5)
+#llama2_llama3_tofu_lrs=(2.5e-5 1.75e-5)
+#gemma_pistol_lrs=(1.5e-5)
+#gemma_tofu_lrs=(1.05e-5)
+#qwen_pistol_lrs=(2e-5)
+#qwen_tofu_lrs=(4e-5)
 
 for j in "${!dataset_names[@]}"; do
   dataset_name=${dataset_names[$j]}
@@ -44,6 +51,7 @@ for j in "${!dataset_names[@]}"; do
 export HF_TOKEN=\$(cat /nfs-share/mk2296/.huggingface_token)
 
 which python
+python forget.py forget.num_run=${num_run} forget.lr=${lr} model_family='${model_family}' pretrained_model_path='${pretrained_model_path}' forget.num_run=${num_run} dataset_name='${dataset_name}' data_path='${data_path}' forget_type='${forget_type}' forget_edge='${forget_edge}'
 python eval.py forget.num_run=${num_run} forget.lr=${lr} model_family='${model_family}' pretrained_model_path='${pretrained_model_path}' forget.num_run=${num_run} dataset_name='${dataset_name}' data_path='${data_path}' forget_type='${forget_type}' forget_edge='${forget_edge}'
 EOT
     done
